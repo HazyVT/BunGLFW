1fce868c0ad9753558feb499ef8c3e6454561e25
Mohammed Salim <renablehots@gmail.com>
2024-01-30 12:29:00 +0400

    GLFW bindings initialized

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import { dlopen, suffix, FFIType, CString, Pointer } from "bun:ffi";
+import { dlopen, suffix, FFIType } from "bun:ffi";
 
-const path = `./glfw3.dll`;
+const platform = process.platform;
+let path: string = "";
+
+if (platform == "darwin") {
+  path = `./libglfw.3.${suffix}`;
+} else if (platform == "win32") {
+  path = `./glfw3.${suffix}`;
+}
 
 const lib = dlopen(path, {
   glfwInit: {
@@ -21,6 +28,22 @@ const lib = dlopen(path, {
   glfwShowWindow: {
     args: ["ptr"],
     returns: "void"
+  },
+  glfwWindowShouldClose: {
+    args: ["ptr"],
+    returns: "int"
+  },
+  glfwPollEvents: {
+    args: [],
+    returns: "void"
+  },
+  glfwSwapBuffers: {
+    args: ["ptr"],
+    returns: "void"
+  },
+  glfwSwapInterval: {
+    args: ["int"],
+    returns: "void"
   }
 })
 
@@ -28,11 +51,22 @@ const init = lib.symbols.glfwInit;
 const create = lib.symbols.glfwCreateWindow;
 const glfwMakeContextCurrent = (window) => lib.symbols.glfwMakeContextCurrent(window);
 const glfwShowWindow = (window) => lib.symbols.glfwShowWindow(window);
+const glfwWindowShouldClose = (window) => lib.symbols.glfwWindowShouldClose(window);
+const glfwSwapBuffers = (window) => lib.symbols.glfwSwapBuffers(window);
+const glfwPollEvents = lib.symbols.glfwPollEvents;
+const glfwSwapInterval = (interval: number) => lib.symbols.glfwSwapInterval(interval);
 
 if (!init()) {
   console.error("Failed to init");
 }
 const window = create(640, 480, Buffer.from("Hello World"), null, null);
 glfwMakeContextCurrent(window);
+glfwSwapInterval(1);
 glfwShowWindow(window);
+
+while (!glfwWindowShouldClose(window)) {
+  glfwPollEvents();
+
+  glfwSwapBuffers(window);
+}
 
